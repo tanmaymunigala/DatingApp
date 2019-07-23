f397c4e96ed18b9bedea49e1d2e3b69ff611e257
tanmaymunigala <tanmay.munigala@gmail.com>
2019-07-23 19:56:22 +1000

    Added Register and Login front-end implementation

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -61,7 +61,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyMethod());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseAuthentication();
             app.UseMvc();
         }
